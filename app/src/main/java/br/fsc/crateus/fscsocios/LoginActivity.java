4d8180a8cdea4a15e26440859a0f99da0ed19951
jandermartins <jandermartins@alu.ufc.br>
2019-12-13 00:31:13 -0300

    autenticação feita

--- app/src/main/java/br/fsc/crateus/fscsocios/LoginActivity.java
+++ app/src/main/java/br/fsc/crateus/fscsocios/LoginActivity.java
@@ -1,15 +1,27 @@
 package br.fsc.crateus.fscsocios;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btEntrarLogin;;
+    Button btEntrarLogin;
+    private FirebaseAuth mAuth;
+    EditText etEmailLogin, etSenhaLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +29,43 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         btEntrarLogin = (Button) findViewById(R.id.btEntrarLogin);
 
+        etEmailLogin = (EditText) findViewById(R.id.etEmailLogin);
+        etSenhaLogin = (EditText) findViewById(R.id.etSenhaLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
         btEntrarLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                mAuth.signInWithEmailAndPassword(etEmailLogin.getText().toString(),
+                        etSenhaLogin.getText().toString()).addOnCompleteListener(LoginActivity.this, new
+                        OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("top", "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("top", "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                            }
+                        });
             }
         });
+    }
+
+    private void updateUI(FirebaseUser firebaseUser) {
+        if(firebaseUser != null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }else {
+            startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+        }
     }
 }
---
+++ app/src/main/java/br/fsc/crateus/fscsocios/Socio.java
@@ -0,0 +1,107 @@
+package br.fsc.crateus.fscsocios;
+
+import java.util.Date;
+
+public class Socio {
+
+    private String idSocio;
+    private String nome;
+    private String CPF;
+    private String RG;
+    private String endereco;
+    private Date nascimento;
+    private String urlFoto3x4;
+    private String idNucleo;
+
+    public Socio(){
+
+    }
+
+    public Socio(String idSocio, String nome, String CPF, String RG, String endereco, Date nascimento, String urlFoto3x4, String idNucleo) {
+        this.idSocio = idSocio;
+        this.nome = nome;
+        this.CPF = CPF;
+        this.RG = RG;
+        this.endereco = endereco;
+        this.nascimento = nascimento;
+        this.urlFoto3x4 = urlFoto3x4;
+        this.idNucleo = idNucleo;
+    }
+
+    @Override
+    public String toString() {
+        return "Socio{" +
+                "idSocio='" + idSocio + '\'' +
+                ", nome='" + nome + '\'' +
+                ", CPF='" + CPF + '\'' +
+                ", RG='" + RG + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", nascimento=" + nascimento +
+                ", urlFoto3x4='" + urlFoto3x4 + '\'' +
+                '}';
+    }
+
+    public String getIdNucleo() {
+        return idNucleo;
+    }
+
+    public void setIdNucleo(String idNucleo) {
+        this.idNucleo = idNucleo;
+    }
+
+    public String getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(String idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getUrlFoto3x4() {
+        return urlFoto3x4;
+    }
+
+    public void setUrlFoto3x4(String urlFoto3x4) {
+        this.urlFoto3x4 = urlFoto3x4;
+    }
+}
