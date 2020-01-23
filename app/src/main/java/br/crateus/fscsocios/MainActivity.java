07c2b6535a4ef82970e626ac642bf7f246663101
jandermartins <jandermartins@alu.ufc.br>
2020-01-23 20:41:26 -0300

    ajustes

--- app/src/main/java/br/crateus/fscsocios/MainActivity.java
+++ app/src/main/java/br/crateus/fscsocios/MainActivity.java
@@ -7,14 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     Button login;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            login.setVisibility(View.INVISIBLE);
+        }
 
         login = (Button) findViewById(R.id.btLogin);
 
