66efa87a1b7a7f99c12810f44d46b080d9d741ce
jandermartins <jandermartins@alu.ufc.br>
2020-01-25 17:47:16 -0300

    model socios criado

--- app/src/main/java/br/crateus/fscsocios/activity/MainActivity.java
+++ app/src/main/java/br/crateus/fscsocios/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package br.crateus.fscsocios.activitys;
+package br.crateus.fscsocios.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/br/crateus/fscsocios/model/Socio.java
+++ app/src/main/java/br/crateus/fscsocios/model/Socio.java
@@ -1,4 +1,15 @@
 package br.crateus.fscsocios.model;
 
+import java.util.Date;
+
 public class Socio {
+
+    private String id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private Date nascimento;
+    private String Cpf;
+    private String Rg;
+    private String raca;
 }
